8004c43bc828c3063989be55d183ec4277ed1f14
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-01-16 16:18:43 -0500

    init
    - practice making mock e-commerce website with node, express and pub

---
+++ index.js
@@ -0,0 +1,42 @@
+//import required modules
+const express = require("express");
+const path = require("path");
+
+//set up Express object and port
+const app = express();
+const port = process.env.PORT || "8888";
+
+//define important folders
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+//setup public folders
+app.use(express.static(path.join(__dirname, "public")));
+
+var links = [
+  {
+    name: "Home",
+    path: "/index",
+  },
+  {
+    name: "Sign In",
+    path: "/",
+  },
+  {
+    name: "Contact Us",
+    path: "/contact-us",
+  },
+];
+
+//Express app
+app.get("/index", (req, res) => {
+  res.render("index", { title: "Home", menu: links });
+});
+app.get("/contact-us", (req, res) => {
+  res.render("contact-us", { title: "Contact Us", menu: links });
+});
+
+//set up server listening
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
